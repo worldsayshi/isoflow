224e8f5e20a4fc368ba55949928a4a7d08b69efd
worldsayshi <per.fredelius+git@gmail.com>
2025-07-15 17:38:23 +0200

    rename indicator

--- src/hooks/useNodeIndicatorComponent.ts
+++ src/hooks/useNodeIndicatorComponent.ts
@@ -1,8 +1,5 @@
-import { useMemo } from 'react';
-import { ModelItem, NodeIndicatorComponentProps } from 'src/types';
-import { useModelStore } from 'src/stores/modelStore';
-import { getItemByIdOrThrow } from 'src/utils';
 import { useUiStateStore } from 'src/stores/uiStateStore';
+import { NodeIndicatorComponentProps } from 'src/types';
 
 export const useNodeIndicatorComponent = (id: string): ((props: NodeIndicatorComponentProps) => JSX.Element) => {
   const model = useUiStateStore((state) => {
