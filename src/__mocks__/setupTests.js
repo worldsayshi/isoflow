30dff6a1c47d5104b38c36d4280a1000a5a8a0fb
worldsayshi <per.fredelius+git@gmail.com>
2025-09-07 16:26:10 +0200

    tests working

---
+++ src/__mocks__/setupTests.js
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom';
+
+// Mock ResizeObserver which is not available in jsdom
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
